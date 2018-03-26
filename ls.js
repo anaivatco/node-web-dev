77146852920dec6a1fc80ce3b969ca0c6a3c2154
anaivatco <anaivatco@yahoo.com>
2018-03-26 08:18:25 +0700

    test2

--- ls.js
+++ ls.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
-var dir = '.';
-if (process.argv[2]) dir = process.argv[2];
-var files = fs.readdirSync(dir);
+var files = fs.readdirSync('.');
 for (fn in files) {
 console.log(files[fn]);
 }
