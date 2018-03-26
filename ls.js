de1cad8463e70b27ce9b41bab9568de778b47e3c
anaivatco <anaivatco@yahoo.com>
2018-03-26 08:17:08 +0700

    tesst

--- ls.js
+++ ls.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-var files = fs.readdirSync('.');
+var dir = '.';
+if (process.argv[2]) dir = process.argv[2];
+var files = fs.readdirSync(dir);
 for (fn in files) {
 console.log(files[fn]);
 }
